bc35351a05367d78e21526f74b280b6458ff2889
Jon Eubank <joneubank@gmail.com>
2021-08-22 21:07:44 -0400

    Add sequence and clamp utility methods

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,21 @@
 import { TAU } from './constants';
 
+export function clamp(
+  value: number,
+  options: { min: number; max: number } = { min: 0, max: 1 },
+) {
+  return Math.max(options.min, Math.min(options.max, value));
+}
+
 // Arrays and Iteration
 export function array(length: number) {
   return Array.from({ length: length }, (_v, k) => k);
 }
 export function range(start: number, end: number) {
   return array(end - start).map((i) => i + start);
+}
+export function sequence<T>(count: number, method: (i: number) => T): T[] {
+  return array(count).map((i) => method(i));
 }
 export function repeat(count: number, action: (index: number) => void): void {
   for (let i = 0; i <= count; i++) {
