8e8987758849d1dc25d91a3930b11a2361f3d96d
Jon Eubank <joneubank@gmail.com>
2021-09-04 11:31:31 -0400

    Cleanup Vec2 export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as Utils from './utils';
 import Canvas from './canvas';
 import Color from './color';
 import Random from './random';
-import Vec2 from './math/Vec2';
+import { Vec2 } from './math';
 
 export { Canvas, Color, Constants, Random, Utils, Vec2 };
