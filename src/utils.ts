4de8fc12569d542c7679490e9cbf0395343868e5
Jon Eubank <joneubank@gmail.com>
2021-09-18 12:17:20 -0400

    Add jsdocs to `clamp`

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,18 @@
 import { TAU } from './constants';
 
+/**
+ * Restricts a value to within a defined range (default is 0 to 1).
+ * If the input number is outside this range, then the min or max value of that
+ * range wil be returned in its place. If the input number is within the range,
+ * the original value will be returned.
+ * @param value {number}
+ * @param options {Object}
+ * @returns {number}
+ */
 export function clamp(
   value: number,
   options: { min: number; max: number } = { min: 0, max: 1 },
-) {
+): number {
   return Math.max(options.min, Math.min(options.max, value));
 }
 
