f07c1ecf414020c433380baef96571d80874a989
Jon Eubank <joneubank@gmail.com>
2021-09-19 13:55:40 -0400

    Add two common distributions

--- src/random/distributions/index.ts
+++ src/random/distributions/index.ts
@@ -1,5 +1,7 @@
 import Distribution from './Distribution';
 
 export { default as Uniform } from './Uniform';
+export { default as Power } from './Power';
+export { default as Sin } from './Sin';
 
 export default Distribution;
