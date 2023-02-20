68ec8e67b98131f31dda1fe3cb9dc12d7baf95c1
Jon Eubank <joneubank@gmail.com>
2023-02-20 15:11:46 -0500

    Method to break out of repeat loop

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -7,8 +7,16 @@ export function range(start: number, end: number) {
 export function sequence<T>(count: number, method: (i: number) => T): T[] {
   return array(count).map((i) => method(i));
 }
-export function repeat(count: number, action: (index: number) => void): void {
+export function repeat(
+  count: number,
+  action: (index: number, stopLoop: () => void) => void,
+): void {
   for (let i = 0; i < count; i++) {
-    action(i);
+    let stopRequested = false;
+    const stopLoop = () => (stopRequested = true);
+    action(i, stopLoop);
+    if (stopRequested) {
+      break;
+    }
   }
 }
